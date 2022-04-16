e4e9d2e63e0882b171c6432baec2da67cf188c73
adapkus <70859104+adapkus@users.noreply.github.com>
2022-04-16 12:48:39 -0400

    Add files via upload

---
+++ Testing/Website_Register_No_Period_After_At.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.Random;
+
+public class Website_Register_No_Period_After_At {
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\aadap\\eclipse\\chromedriver.exe");
+		
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://web.ics.purdue.edu/~g1120478/register_1.html");
+	    Random rand = new Random(); //instance of random class
+	    int int_random = rand.nextInt(); 
+	    
+		driver.findElement(By.id("username")).sendKeys("WebTestNo" + Integer.toString(int_random));
+		driver.findElement(By.id("password")).sendKeys("12345");
+		driver.findElement(By.id("email")).sendKeys("test@asd");
+		driver.findElement(By.xpath("/html/body/div/form/input[4]")).click();
+		String u = driver.getCurrentUrl();
+		if (!u.equals("https://web.ics.purdue.edu/~g1120478/index.html")) {
+			System.out.println("Test Case Passed");
+		}
+		else {
+			System.out.println("Test Case Failed");
+		}
+		driver.close();
+	}
+}
